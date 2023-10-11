6b79e00ebd8b90c96ea4ae751b688325f2faeb25
yrodr <y.rodr1gues0811@outlook.com>
2023-10-11 12:38:44 -0300

    category class, repository, service and resource

---
+++ src/main/java/com/rodrigues/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.rodrigues.course.repositories;
+
+import com.rodrigues.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
