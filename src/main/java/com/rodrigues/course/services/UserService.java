855255d1d20b582d21fe10eb10d9696131527842
yrodr <y.rodr1gues0811@outlook.com>
2023-10-16 14:38:05 -0300

    exception handler - findById

--- src/main/java/com/rodrigues/course/services/UserService.java
+++ src/main/java/com/rodrigues/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.rodrigues.course.services;
 
 import com.rodrigues.course.entities.User;
 import com.rodrigues.course.repositories.UserRepository;
+import com.rodrigues.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,9 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj =repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(
+                () -> new ResourceNotFoundException(id)
+        );
     }
 
     public User insert(User obj) {
